2519d0405151112cdc6a2db9d3d3ce1b27fec3f1
eriks1995@gmail.com <eriks1995@gmail.com>
2017-01-31 15:45:20 +0200

    Samba install - OK

--- Kodutoo1.sh
+++ Kodutoo1.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-sudo dpkg -s samba > /dev/null
-echo $?
+
+# kõikide järgnevate käskude stdout ja stderr saadetakse /dev/nulli
+
+# kontrollib, kas samba on installitud
+sudo dpkg -s samba > /dev/null 2>&1
+
+# kui eelneva käsu veakood pole 0 (samba pole installitud)
 if [ $? != 0 ]
 	then
-	echo '2'
-	sudo apt -y install samba > /dev/null
-	if [$? != 0]
+	sudo apt -y install samba > /dev/null 2>&1
+	
+	# kui samba installimisel esines tõrge
+	if [ $? != 0 ]
 		then
 		echo 'Viga Samba paigaldamisel!'
 		exit 1
 	fi
-	echo '3'
 fi
+
 
