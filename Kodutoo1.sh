55a9e4e559470b0c9e24c52eee010c435e5ef8fe
eriks1995@gmail.com <eriks1995@gmail.com>
2017-01-31 17:38:17 +0200

    Kausta paigaldus - OK

--- Kodutoo1.sh
+++ Kodutoo1.sh
@@ -18,4 +18,18 @@ if [ $? != 0 ]
 	fi
 fi
 
+# kontrollib, kas kaust on juba olemas
+test -d $1
 
+# juhul, kui eelneva käsu veakood pole 0 (kausta pole olemas)
+if [ $? != 0 ]
+	then
+	sudo mkdir -p $1
+	
+	# kui kausta loomisel esines tõrge
+	if [ $? != 0 ]
+		then
+		echo 'Viga kausta loomisel!'
+		exit 1
+	fi
+fi
